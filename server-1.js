70b1b2c32d74135004e2e2cfbc86115912d14f76
mdalaminhossain1078-lgtm <mdalaminhossain1078@gmail.com>
2026-06-09 09:06:49 +0600

    Update server-1.js

--- server-1.js
+++ server-1.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('.'));
 
 app.post('/api/chat', async (req, res) => {
   try {
